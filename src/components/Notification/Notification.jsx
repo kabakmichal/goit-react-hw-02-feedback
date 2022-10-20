9a5c5b11d560bcfa448ddb99bcd63041c7effc7e
kabakmichal <99827389+kabakmichal@users.noreply.github.com>
2022-10-20 17:54:52 +0200

    Message in Notification

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,10 +1,9 @@
 import PropTypes from 'prop-types';
 import NotificationCSS from './Notification.module.css';
 
-export const Notification = ({ message }) => {
-  <p className={NotificationCSS.paragraph}>{message}</p>;
-};
-
+export const Notification = ({ message }) => (
+  <p className={NotificationCSS.paragraph}>{message}</p>
+);
 
 Notification.propTypes = {
   message: PropTypes.string.isRequired,
